371fc913ed0e47cb9b5a935c72027b7965cddea8
mangofunky <mangofunky@hotmail.com>
2022-07-30 13:17:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "networking" {
   vpc_cidr = "10.123.0.0/16"
   public_sn_count = 2
   private_sn_count = 3
-  public_cidrs = [for i in range(2,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
-  private_cidrs = [for i in range(1,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
+  public_cidr = [for i in range(2,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
+  private_cidr = [for i in range(1,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
 }
