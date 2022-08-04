bbdf37d980dac87655316c28f76dba1efef5b38e
G.Mangano <mangofunky@hotmail.com>
2022-08-04 12:26:05 +0100

    Public SN count change

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 module "networking" {
   source   = "./networking"
   vpc_cidr = "10.123.0.0/16"
-  public_sn_count = 1
-  private_sn_count = 1
+  public_sn_count = 2
+  private_sn_count = 2
   public_cidr = [for i in range(2,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
   private_cidr = [for i in range(1,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
 }
