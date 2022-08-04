7ca4afb4fd0fbd620569c4bf433a3f348fd8a98c
G.Mangano <mangofunky@hotmail.com>
2022-08-04 13:41:04 +0100

    add ipv6

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 module "networking" {
   source   = "./networking"
   vpc_cidr = "10.123.0.0/16"
-  ipv6_cidr_block = "::/0"
+  #ipv6_cidr_block = "::/0"
   public_sn_count = 3
   private_sn_count = 4
   public_cidr = [for i in range(2,255,2) : cidrsubnet("10.123.0.0/16", 8 , i)]
